8c5e2db5a7903b00dbd67a4b68fe687bcf82bcde
Yumiko Yamasaki <y.yamasaki@gifted.academy>
2017-09-23 20:32:33 +0900

    first commit

---
+++ schoo_shibucafe_file/shibucafeJq/shibucafe.js
@@ -0,0 +1,28 @@
+//滑らかに遷移させる//
+$(function(){
+   // #で始まるアンカーをクリックした場合に処理
+   $('a[href^="#"]').click(function() {
+      // スクロールの速度
+      var speed = 400; // ミリ秒
+      // アンカーの値取得
+      var href= $(this).attr("href");
+      // 移動先を取得
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      // 移動先を数値で取得
+      var position = target.offset().top;
+      // スムーススクロール
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
+});
+
+/*ドロワーメニュー*/
+$('#open').click(function(){
+  $(this).toggleClass('opening')
+});
+$('#open').click(function(){
+  $('#menu ul').toggleClass('showMenue')
+});
+$('#menu ul li a').click(function(){
+  $('#menu ul').toggleClass('showMenue')
+});
